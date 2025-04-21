b9fe725a266e13477453179ae8148c3462c887a1
d3n2dboy <d3n2dboy@gmail.com>
2025-04-21 19:56:48 +0500

    Updates

--- js/app.js
+++ js/app.js
@@ -3672,6 +3672,25 @@
                     prevEl: ".swiper-button-prev",
                     nextEl: ".swiper-button-next"
                 },
+                breakpoints: {
+                    320: {
+                        slidesPerView: 1,
+                        spaceBetween: 15
+                    },
+                    768: {
+                        slidesPerView: 2,
+                        spaceBetween: 20,
+                        autoHeight: true
+                    },
+                    992: {
+                        slidesPerView: 3,
+                        spaceBetween: 20
+                    },
+                    1268: {
+                        slidesPerView: 3,
+                        spaceBetween: 30
+                    }
+                },
                 on: {}
             });
         }
